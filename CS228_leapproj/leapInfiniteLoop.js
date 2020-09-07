241cbfff55715e60771bba940a84d2f130f8f972
Tom <tkiely99@gmail.com>
2020-09-07 12:01:47 -0400

    Just completed deliverable 1, step 48

--- CS228_leapproj/leapInfiniteLoop.js
+++ CS228_leapproj/leapInfiniteLoop.js
@@ -1,7 +1,11 @@
 var controllerOptions = {}
 var i =0;
+var x = window.innerWidth;
+var y = window.innerHeight;
 Leap.loop(controllerOptions,function (frame) {
-
+    circle(50,50,50);
     console.log(i);
     i++;
+    
+
 });
