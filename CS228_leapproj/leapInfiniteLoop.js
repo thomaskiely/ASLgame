ae3bcdd37d720f21995106521abce8bb5bacb072
Tom <tkiely99@gmail.com>
2020-09-07 14:06:56 -0400

    Just completed deliverable 1, step 64

--- CS228_leapproj/leapInfiniteLoop.js
+++ CS228_leapproj/leapInfiniteLoop.js
@@ -4,6 +4,11 @@ var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 
 Leap.loop(controllerOptions,function (frame) {
+    HandleFrame(frame);
+
+});
+
+function HandleFrame(frame) {
     /*clear();
     circle(x,y,50);
     var randomIntX = Math.round(Math.random()) * 2 - 1;
@@ -14,15 +19,22 @@ Leap.loop(controllerOptions,function (frame) {
 
     if(frame.hands.length == 1){
         var hand = frame.hands[0];
-        //console.log(hand);
-        var fingers = hand.fingers;
-        //console.log(fingers.length);
-        for(var i=0; i<fingers.length;++i){
-            if(fingers[i].type == 1){
-                console.log(fingers[1])
-            }
-            //console.log(fingers[i]);
-        }
+        HandleHand(hand);
     }
+}
 
-});
+function HandleHand(hand) {
+    //console.log(hand);
+    var fingers = hand.fingers;
+    //console.log(fingers.length);
+    HandleFinger(fingers);
+}
+
+function HandleFinger(fingers){
+    for(var i=0; i<fingers.length;++i){
+        if(fingers[i].type == 1){
+            console.log(fingers[1])
+        }
+        //console.log(fingers[i]);
+    }
+}
