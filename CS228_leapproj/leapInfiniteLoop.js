ed0df29dc7a0ecd522f0798f19b96690b33fb91c
Tom <tkiely99@gmail.com>
2020-09-07 12:58:01 -0400

    Just completed deliverable 1, step 54

--- CS228_leapproj/leapInfiniteLoop.js
+++ CS228_leapproj/leapInfiniteLoop.js
@@ -4,9 +4,12 @@ var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 
 Leap.loop(controllerOptions,function (frame) {
-    clear();
+    /*clear();
     circle(x,y,50);
-    var randomInt = Math.round(Math.random()) * 2 - 1
-    x+= randomInt;
+    var randomIntX = Math.round(Math.random()) * 2 - 1;
+    var randomIntY = Math.round(Math.random()) * 2 - 1;
+    x+= randomIntX;
+    y+= randomIntY;*/
+    console.log(frame.hands);
 
 });
