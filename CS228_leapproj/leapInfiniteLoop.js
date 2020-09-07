b5d6fa32c9e8ccb366e56ea10123501963084997
Tom <tkiely99@gmail.com>
2020-09-07 16:38:02 -0400

    changing some things around pushing so nothing gets messed up

--- CS228_leapproj/leapInfiniteLoop.js
+++ CS228_leapproj/leapInfiniteLoop.js
@@ -3,6 +3,11 @@ var controllerOptions = {}
 var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 var z;
+
+var rawXMin = 1000;
+var rawYMin = 1000;
+var rawXMax = -1000;
+var rawYMax = -1000;
 Leap.loop(controllerOptions,function (frame) {
     clear();
     HandleFrame(frame);
@@ -12,6 +17,8 @@ Leap.loop(controllerOptions,function (frame) {
 });
 
 function HandleFrame(frame) {
+    scaleValue(x,0,window.innerWidth);
+    scaleValue(y,0,window.innerHeight);
     circle(x,y,50);
     /*var randomIntX = Math.round(Math.random()) * 2 - 1;
     var randomIntY = Math.round(Math.random()) * 2 - 1;
@@ -42,6 +49,31 @@ function HandleFinger(fingers){
         //console.log(fingers[i]);
     }
     x = fingers[1].tipPosition[0]+300;
-    y = fingers[1].tipPosition[1]+300;
+    y = window.innerHeight-fingers[1].tipPosition[1]+300;
     z = fingers[1].tipPosition[2];
+
+    if(fingers[1].tipPosition[0] < rawXMin){
+        rawXMin = x;
+        console.log(x);
+    }
+    if(window.innerHeight-fingers[1].tipPosition[1] < rawYMin){
+        rawYMin = y;
+        console.log(y);
+    }
+    if(fingers[1].tipPosition[0] > rawXMax){
+        rawXMax = x;
+        console.log(x);
+    }
+    if(window.innerHeight-fingers[1].tipPosition[1] > rawYMax){
+        rawXMin = y;
+        console.log(y);
+    }
+
+
+}
+
+function scaleValue(value, from, to) {
+    var scale = (to[1] - to[0]) / (from[1] - from[0]);
+    var capped = Math.min(from[1], Math.max(from[0], value)) - from[0];
+    return ~~(capped * scale + to[0]);
 }
