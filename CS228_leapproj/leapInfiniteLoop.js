8ec0344653ff14ec742cd5303894bfe71fff8292
Tom <tkiely99@gmail.com>
2020-09-07 19:20:33 -0400

    Just completed deliverable 1, step 75

--- CS228_leapproj/leapInfiniteLoop.js
+++ CS228_leapproj/leapInfiniteLoop.js
@@ -17,8 +17,7 @@ Leap.loop(controllerOptions,function (frame) {
 });
 
 function HandleFrame(frame) {
-    scaleValue(x,0,window.innerWidth);
-    scaleValue(y,0,window.innerHeight);
+
     circle(x,y,50);
     /*var randomIntX = Math.round(Math.random()) * 2 - 1;
     var randomIntY = Math.round(Math.random()) * 2 - 1;
@@ -32,47 +31,52 @@ function HandleFrame(frame) {
     }
 }
 
+
+
 function HandleHand(hand) {
-    //console.log(hand);
+
     var fingers = hand.fingers;
-    //console.log(fingers.length);
 
-    HandleFinger(fingers);
-}
-
-function HandleFinger(fingers){
-    for(var i=0; i<fingers.length;++i){
-        if(fingers[i].type == 1){
-            console.log(fingers[1].tipPosition)
+    for(var i=0; i<fingers.length;++i) {
+        if (fingers[i].type == 1) {
+            HandleFinger(fingers[i]);
 
         }
-        //console.log(fingers[i]);
     }
-    x = fingers[1].tipPosition[0]+300;
-    y = window.innerHeight-fingers[1].tipPosition[1]+300;
-    z = fingers[1].tipPosition[2];
+}
 
-    if(fingers[1].tipPosition[0] < rawXMin){
-        rawXMin = x;
+function HandleFinger(finger){
+
+        //console.log(fingers[i]);
+
+    x = finger.tipPosition[0];
+    y = window.innerHeight-finger.tipPosition[1];
+    z = finger.tipPosition[2];
+
+    scale(finger.tipPosition[0],[rawXMin,rawXMax],[0,window.innerWidth]);
+    scale(finger.tipPosition[1],[rawYMin,rawYMax],[0,window.innerHeight]);
+
+    if(finger.tipPosition[0] < rawXMin){
+        rawXMin = finger.tipPosition[0];
         console.log(x);
     }
-    if(window.innerHeight-fingers[1].tipPosition[1] < rawYMin){
-        rawYMin = y;
+    if(finger.tipPosition[1] < rawYMin){
+        rawYMin = y
         console.log(y);
     }
-    if(fingers[1].tipPosition[0] > rawXMax){
-        rawXMax = x;
+    if(finger.tipPosition[0] > rawXMax){
+        rawXMax = finger.tipPosition[0];
         console.log(x);
     }
-    if(window.innerHeight-fingers[1].tipPosition[1] > rawYMax){
-        rawXMin = y;
+    if(finger.tipPosition[1] > rawYMax){
+        rawYMax = y
         console.log(y);
     }
 
 
 }
 
-function scaleValue(value, from, to) {
+function scale(value, from, to) {
     var scale = (to[1] - to[0]) / (from[1] - from[0]);
     var capped = Math.min(from[1], Math.max(from[0], value)) - from[0];
     return ~~(capped * scale + to[0]);
