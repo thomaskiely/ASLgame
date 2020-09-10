e8d3a0598e35d9c296d00ccbc3ef5f38d87c81c0
Tom <tkiely99@gmail.com>
2020-09-10 17:02:00 -0400

    completed step 75 deliverable 1

--- CS228_leapproj/leapInfiniteLoop.js
+++ CS228_leapproj/leapInfiniteLoop.js
@@ -4,25 +4,20 @@ var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 var z;
 
-var rawXMin = 1000;
-var rawYMin = 1000;
-var rawXMax = -1000;
-var rawYMax = -1000;
+var rawXMin = 100000;
+var rawYMin = 100000;
+var rawXMax = -100000;
+var rawYMax = -100000;
 Leap.loop(controllerOptions,function (frame) {
     clear();
     HandleFrame(frame);
-
-
 
 });
 
 function HandleFrame(frame) {
 
     circle(x,y,50);
-    /*var randomIntX = Math.round(Math.random()) * 2 - 1;
-    var randomIntY = Math.round(Math.random()) * 2 - 1;
-    x+= randomIntX;
-    y+= randomIntY;*/
+
 
 
     if(frame.hands.length == 1){
@@ -47,37 +42,61 @@ function HandleHand(hand) {
 
 function HandleFinger(finger){
 
-        //console.log(fingers[i]);
-
-    x = finger.tipPosition[0]+300;
-    y = window.innerHeight-finger.tipPosition[1];
-   // y = finger.tipPosition[1]+300;
-    z = finger.tipPosition[2];
-
-    circle(x,y,50);
-    convertRange(x,[rawXMin,rawXMax],[0,window.innerWidth]);
-    convertRange(y,[rawYMin,rawYMax],[0,window.innerHeight]);
 
     if(finger.tipPosition[0] < rawXMin){
         rawXMin = finger.tipPosition[0];
-        console.log(x);
+        //console.log("xmin"+rawXMin);
     }
     if(finger.tipPosition[1] < rawYMin){
         rawYMin = finger.tipPosition[1];
-        console.log(y);
+        //console.log("ymin"+rawYMin);
     }
     if(finger.tipPosition[0] > rawXMax){
         rawXMax = finger.tipPosition[0];
-        console.log(x);
+        // console.log("xmax"+rawXMax);
     }
     if(finger.tipPosition[1] > rawYMax){
         rawYMax = finger.tipPosition[1];
-        console.log(y);
     }
 
+    x = scaleValue(finger.tipPosition[0],[rawXMin,rawXMax],[0,window.innerWidth]);
+    y = window.innerHeight - scaleValue(finger.tipPosition[1],[rawYMin,rawYMax],[0,window.innerHeight]);
+    z = finger.tipPosition[2];
+
+
+
+    //circle(x,y,50);
+
+
 
 }
+
+
+/*function convertRange( value, r1, r2 ) {
+    //oldval
+    //newmin
+    //r1[0]= old min
+    //r1[1]= old max
+    //r2[0] = new min
+    //r2[1] = new max
+    var oldRange = (r1[1] - r1[0])
+    var newRange = (r2[1] - r2[0])
+    var newValue = (((value - r1[0]) * newRange) / oldRange) + r2[0];
+    return newValue;
+}*/
+
+//should be between raw min,x,rawmax
+//new min, new x, new max
 
 function convertRange( value, r1, r2 ) {
+    //(finger position - RawMin)*(window.innerheight - 0) / (RawMax-rawMin)+0
+
     return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
 }
+
+function scaleValue(value, from, to) {
+    var scale = (to[1] - to[0]) / (from[1] - from[0]);
+    var capped = Math.min(from[1], Math.max(from[0], value)) - from[0];
+    return ~~(capped * scale + to[0]);
+}
+
