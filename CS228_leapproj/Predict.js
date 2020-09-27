d9223eae9f83f1606d34022055d33ca86b32e2fe
Tom <tkiely99@gmail.com>
2020-09-27 14:13:26 -0400

    completed Deliverable 4 step 9

--- CS228_leapproj/Predict.js
+++ CS228_leapproj/Predict.js
@@ -1,12 +1,23 @@
 
 
 
-
+var trainingCompleted = false;
 function draw(){
 
     clear();
+    if (trainingCompleted == false){
+        Train();
+    }
 
+    Test();
     
+}
 
+function Train(){
+    console.log("I am being trained");
+    trainingCompleted = true;
+}
 
+function Test() {
+    console.log("I am being tested");
 }
