99146851aca2136c2db7011540c3765db986e02e
Tom <tkiely99@gmail.com>
2020-10-26 14:57:42 -0400

    completed Deliverable 7 step 38

--- CS228_leapproj/prepareToDraw.js
+++ CS228_leapproj/prepareToDraw.js
@@ -1,5 +1,23 @@
+var img;
+var imgCenter;
+var imgRight;
+var imgLeft;
+var imgUp;
+var imgDown;
+var imgToward;
+var imgAway;
 function setup(){
 
     createCanvas(window.innerWidth, window.innerHeight);
+    img = loadImage('https://i.imgur.com/71IQGOo.jpg');
+    imgCenter = loadImage('https://i.imgur.com/EokGxpz.jpg');
+    imgRight = loadImage('https://i.imgur.com/I2GycZp.jpg');
+    imgLeft = loadImage('https://i.imgur.com/3F860Pe.jpg');
+    imgUp = loadImage('https://i.imgur.com/o7LZjxH.jpg');
+    imgDown = loadImage('https://i.imgur.com/gBuKd5m.jpg');
+    imgToward = loadImage('https://i.imgur.com/Cmh0BJu.jpg');
+    imgAway = loadImage('https://i.imgur.com/ch6ynHb.jpg');
+
+
 
 }
